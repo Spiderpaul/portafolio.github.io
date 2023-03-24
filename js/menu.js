6debcdf7723360e8f702f518d803cf045ce0625e
Paul Lugo <paulugortega@gmail.com>
2023-03-24 16:19:50 -0700

    Animación de imagen corregida

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
 const container = document.querySelector("[data-container]");
 const img = document.querySelector("[data-img]");
+const back = document.querySelector("[data-img-back]");
 
 window.addEventListener("scroll", function(){
     container.classList.toggle("active", window.scrollY > 0);
     img.classList.toggle("active", window.scrollY > 0);
+    back.classList.toggle("active", window.scrollY > 0);
 });
